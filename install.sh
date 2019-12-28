0505869006f1def7d5d74316daf1637487dcf9eb
Med Ismail Bennani <medismail.bennani@gmail.com>
2019-12-28 02:56:26 +0100

    Update install script
    Add new brew packages.
    Add new cargo packages.
    Fix brew permissions and ownership.
    Add arcanist installer
    Add debug mode.
    
    Signed-off-by: Med Ismail Bennani <medismail.bennani@gmail.com>

--- install.sh
+++ install.sh
@@ -32,11 +32,46 @@ function brew {
   chmod +x install.rb
   ./install.rb
 
+  sudo chown -R $(whoami) /usr/local/Frameworks     \
+                          /usr/local/bin            \
+                          /usr/local/etc            \
+                          /usr/local/lib            \
+                          /usr/local/sbin           \
+                          /usr/local/share          \
+                          /usr/local/share/doc      \
+                          /usr/local/share/locale   \
+                          /usr/local/share/man      \
+                          /usr/local/share/man/man1 \
+                          /usr/local/share/man/man2 \
+                          /usr/local/share/man/man3 \
+                          /usr/local/share/man/man4 \
+                          /usr/local/share/man/man5 \
+                          /usr/local/share/man/man7 \
+                          /usr/local/share/man/man8
+
+  chmod u+w /usr/local/Frameworks     \
+            /usr/local/bin            \
+            /usr/local/etc            \
+            /usr/local/lib            \
+            /usr/local/sbin           \
+            /usr/local/share          \
+            /usr/local/share/doc      \
+            /usr/local/share/locale   \
+            /usr/local/share/man      \
+            /usr/local/share/man/man1 \
+            /usr/local/share/man/man2 \
+            /usr/local/share/man/man3 \
+            /usr/local/share/man/man4 \
+            /usr/local/share/man/man5 \
+            /usr/local/share/man/man7 \
+            /usr/local/share/man/man8
+
   if [ -d "/usr/local/share/swig" ]; then
     sudo chmod 777 "/usr/local/share/swig"
   fi
 
-  brew_list="tree cmake ninja swig"
+  brew_list="tree cmake ninja swig clang-format htop nmap neofetch tmux
+  youtube-dl vim ctags-exuberant"
   cask_list="authy discord"
 
   brew install "$brew_list"
@@ -49,13 +84,36 @@ function brew {
 function cargo {
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-  crate_list="fd-find ripgrep bat"
+  crate_list="fd-find ripgrep bat tokei"
 
   cargo install $crate_list
 }
 
+function arcanist {
+  arc_list="libphutil arcanist"
+  cellar_path="/usr/local/Cellar"
+  bin_path="/usr/local/bin"
+
+  mkdir -p $cellar_path
+
+  for pkg in $arc_list; do
+    pkg_path=$cellar_path/$pkg
+    if [ ! -d $pkg_path ]; then
+      git clone https://github.com/phacility/$pkg.git $pkg_path
+    else
+      git -C $pkg_path pull origin master
+    fi
+  done
+
+  ln -svf $cellar_path/arcanist/bin/arc $bin_path/arc
+}
+
 platform="$(uname)"
 echo "$platform platform detected!"
+
+if [ ! -z $DEBUG ]; then
+  set -x
+fi
 
 if [ $# == "0" ]; then
   echo "Installing dotfiles"
